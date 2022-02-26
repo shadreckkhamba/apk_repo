b99d5092861bb989e8f58edd883c64e2d97a3a36
shadreck khamba <khambashadreck@gmail.com>
2022-02-26 01:05:19 -0800

    add apk sizes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 import Store from "./components/Store";
 function App() {
-    const StoreItems = ["Whatsapp","Facebook","Instagram"];
+    const StoreItems = [
+        {
+        title: " Whatsapp",
+        size: " 49mb",
+    },
+    {
+        title: " Instagram",
+        size: "70mb",
+    },
+    {
+        title: " Facebook",
+        size: " 40mb",
+    },
+    {
+        title: " Twitter",
+        size: " 45mb",
+    },
+  ];
     return (
         <div>
             <Store items={StoreItems}/>
--- src/components/Store.js
+++ src/components/Store.js
@@ -1,20 +1,24 @@
-import { Box, Heading, Input, Stack, Button } from "@chakra-ui/react";
+import { Box, Heading, Input, Stack, Button, Text, Flex, Spacer } from "@chakra-ui/react";
 
-const StoreItem = ({title})=>{
+const StoreItem = ({title, size})=>{
     return <Box p={4} borderRadius="lg" borderWidth="1px">
+        <Flex alignItems="center">
         <Heading size="md">{title}</Heading>
+        <Spacer/>
+        <Text>{size}</Text>
+        </Flex>
     </Box>
 }
-
+  
 function Store({items}){
-    return <Box>
+    return <Box padding={4}>
         <Stack>
         {items.map(item=>{
-           return <StoreItem title={item}/>
+           return <StoreItem title={item.title} size={item.size}/>
         })}
         </Stack>
-        <Input mt={10} placeholder="application name"/>
-        <Button>Search for applications </Button>
+        <Input mt={10} placeholder="app name"/>
+        <Button mt={2}>Search</Button>
     </Box>
 }
 export default Store;
